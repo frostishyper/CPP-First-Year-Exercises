51cf04cd6bbf1649cfc9dfb02122f4697ed3b0ce
Lee <71824443+frostishyper@users.noreply.github.com>
2024-08-30 09:37:14 +0800

    Commit Task 6-8
    Committing Task 6-8

---
+++ Task #8/Task8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int Num1;
+    int Num2;
+    
+    cout << "Please Enter A Number" << endl << endl;
+    cin >> Num1;
+
+
+    cout << "Please Enter Another Number" << endl << endl; 
+    cin >> Num2;
+
+    cout << Num1 - Num2 << endl << endl;
+
+    system("pause");
+    return 0;
+}
